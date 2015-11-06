1b65a296d00d5e9ac75b9016ce21a6e89a6e9fad
Brian Stolz <brian@tecnobrat.com>
2015-11-06 12:05:57 -0800

    Refactors to make it work with POST, PUT, etc

--- lib/pylon/dispatcher.ex
+++ lib/pylon/dispatcher.ex
@@ -4,18 +4,24 @@ defmodule Pylon.Dispatcher do
   end
 
   def handle(req, state) do
-    jwt_data = parse_auth_header(req)
-    if jwt_data == :invalid do
-      {:ok, req} = :cowboy_req.reply(403, [], '{ error: "Invalid JWT" }', req)
-    else
-      {request_path, _} = :cowboy_req.path(req)
-      request_path_parts = String.split(request_path, "/", parts: 2, trim: true)
-      uri = get_upstream_uri(List.first(request_path_parts))
-
-      [status_code, headers, body] = fetch_api(uri)
-      {:ok, req} = :cowboy_req.reply(status_code, headers, body, req)
+    case verify_jwt(req) do
+      :invalid ->
+        {:ok, _} = :cowboy_req.reply(403, [], '{ error: "Invalid JWT" }', req)
+      _ ->
+        handle_request(req)
     end
     {:ok, req, state}
+  end
+
+  def handle_request(req) do
+    {request_path, _} = :cowboy_req.path(req)
+    request_path_parts = String.split(request_path, "/", parts: 2, trim: true)
+    uri = get_upstream_uri(List.first(request_path_parts))
+    {method, _} = :cowboy_req.method(req)
+    method = String.to_atom(String.downcase(method))
+
+    [status_code, headers, body] = fetch_api(uri, method)
+    {:ok, _} = :cowboy_req.reply(status_code, headers, body, req)
   end
 
   def get_upstream_uri(request_path) do
@@ -27,14 +33,9 @@ defmodule Pylon.Dispatcher do
     uri
   end
 
-  def parse_auth_header(req) do
+  def verify_jwt(req) do
     jwt_secret = "secret"
-
-    claims = {[ {:user_id, 1} ]}
-    expiration_seconds = 86400
-    token = :ejwt.jwt("HS256", claims, expiration_seconds, jwt_secret)
-
-    {:ok, {_, jwt_value}, _} = auth_header = :cowboy_req.parse_header("authorization", req)
+    {:ok, {_, jwt_value}, _} = :cowboy_req.parse_header("authorization", req)
     :ejwt.parse_jwt(jwt_value, jwt_secret)
   end
 
@@ -42,16 +43,23 @@ defmodule Pylon.Dispatcher do
     [404, [], "Not found :("]
   end
 
-  def fetch_api(uri) do
+  def fetch_api(uri, method, body \\ :undefined, headers \\ [], options \\ []) do
     IO.inspect("Fetching: " <> uri)
-    case HTTPoison.get(uri) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        [200, [], body]
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        [404, [], "Not found :("]
+    case execute_api(uri, method, body, headers, options) do
+      {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
+        [status_code, [], body]
       {:error, %HTTPoison.Error{reason: reason}} ->
         [500, [], reason]
     end
+  end
+
+  def execute_api(uri, method, body, headers, options) when method == :post or method == :patch or method == :put do
+    IO.inspect "OMG ITS A POST OR SOMETHING"
+    apply(HTTPoison, method, [uri, body, headers, options])
+  end
+
+  def execute_api(uri, method, body, headers, options) do
+    apply(HTTPoison, method, [uri, headers, options])
   end
 
   def terminate(_reason, _req, _state), do: :ok
